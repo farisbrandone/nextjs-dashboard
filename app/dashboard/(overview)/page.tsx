a9c503583ccd9a9a426682a39e54a4843a7390b7
farisbrandone <farisbrandone@yahoo.com>
2024-06-02 07:43:39 +0100

    mine

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -10,6 +10,7 @@ import {
 } from '@/app/lib/data';
 import { Suspense } from 'react'; // part of streaming component
 import {
+  CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons'; // part of streaming component
@@ -17,14 +18,14 @@ import {
 export default async function Page() {
   /*const revenue = await fetchRevenue(); remove all instance of fetch revenue*/
   const latestInvoices = await fetchLatestInvoices();
-  const cardData = await fetchCardData();
+  //const cardData = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card
+        {/*<Card
           title="Collected"
           value={cardData.totalPaidInvoices}
           type="collected"
@@ -43,7 +44,10 @@ export default async function Page() {
           title="Total Customers"
           value={cardData.numberOfCustomers}
           type="customers"
-        />
+        />*/}
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {/*<RevenueChart revenue={revenue} />
